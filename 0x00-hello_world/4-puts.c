5ea2ad779322a176cf67f88d1c73e4f6be2aef0d
Erick Saddam <ericksaddam@outlook.com>
2022-03-10 05:06:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /*
  * main - Entry point
  *
- * Description:A C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line."
+ * Description: A C program to print "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
