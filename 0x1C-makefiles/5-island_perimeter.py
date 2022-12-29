e6214fb358e286cf0d952a3d2cf3c97b1dd34caa
Erick Saddam <ericksaddam3@outlook.com>
2022-12-29 09:18:40 +0300

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,26 +1,45 @@
 #!/usr/bin/python3
-""" Module that contains the island_perimeter function """
 
 
 def island_perimeter(grid):
-    """ Function that returns the perimeter of island described in grid """
-    perimeter = 0
+    """ Function that returns detail of a grid
+    """
 
-    nrows = len(grid)
+    if not isinstance(grid, list) or not len(grid):
+        return 0
+    y = len(grid) - 1
+    x = len(grid[0]) - 1
 
-    if grid != []:
-        ncolumns = len(grid[0])
+    prim = 0
 
-    for a in range(nrows):
-        for b in range(ncolumns):
-            if grid[a][b] == 1:
-                if (a - 1) == -1 or grid[a - 1][b] == 0:
-                    perimeter += 1
-                if (a + 1) == nrows or grid[a + 1][b] == 0:
-                    perimeter += 1
-                if (b - 1) == -1 or grid[a][b - 1] == 0:
-                    perimeter += 1
-                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
-                    perimeter += 1
+    for i in range(y):
+        for j in range(x):
+            if grid[i][j] == 1:
+                prim += check_surround(grid, i, j)
+    return prim
 
-    return perimeter
+
+def check_surround(grid, row, idx):
+    """ Function that checks the surroundings of a given tile
+    """
+    height = len(grid)
+    length = len(grid[0])
+    sur = [0, 0, 0, 0]
+
+    if row - 1 >= 0:  # up
+        if not grid[row - 1][idx]:
+            sur[0] = 1
+
+    if row + 1 < height:  # down
+        if not grid[row + 1][idx]:
+            sur[1] = 1
+
+    if idx - 1 >= 0:  # left
+        if not grid[row][idx - 1]:
+            sur[2] = 1
+
+    if idx + 1 < length:  # right
+        if not grid[row][idx + 1]:
+            sur[3] = 1
+
+    return sum(sur)
